d256cdb0a838164f679305b27d917ddae7e5e47c
m-n-zuk <magdalena.n.zuk@gmail.com>
2024-04-06 19:22:01 +0200

    add steps from chapter 1 - 6

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'First steps in Odoo',
+    'license': 'LGPL-3',  # czy to musi byc?
+
+    'depends': [
+        'base_setup',
+    ],
+
+    'data': [
+        'views/ir.model.access.csv',
+
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml',
+
+    ],
+
+    'application': True,
+}
